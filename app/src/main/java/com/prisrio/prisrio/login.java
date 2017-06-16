3031c81e1faa2656763cc204957d049a9fff89e0
hanselblack <hanselblack@hotmail.com>
2017-06-16 17:56:36 +0800

    Testing to get JSON object from Facebook SDK

--- app/src/main/java/com/prisrio/prisrio/login.java
+++ app/src/main/java/com/prisrio/prisrio/login.java
@@ -10,14 +10,19 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -25,16 +30,20 @@ import java.util.Arrays;
 
 public class login extends AppCompatActivity {
     CallbackManager callbackManager;
+    AccessToken accessToken;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        FacebookSdk.sdkInitialize(getApplicationContext());
+        //To get access token from application
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        accessToken = AccessToken.getCurrentAccessToken();
+
         callbackManager = CallbackManager.Factory.create();
 
         LoginButton authButton = (LoginButton)this.findViewById(R.id.login_button);
-        authButton.setReadPermissions(Arrays.asList("user_status"));
+        authButton.setReadPermissions(Arrays.asList("user_status","user_friends"));
         
         
         LoginManager.getInstance().registerCallback(callbackManager,
@@ -42,7 +51,24 @@ public class login extends AppCompatActivity {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
                         // App code
-                        goMainMenu();
+
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                        accessToken,
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(
+                                            JSONObject object,
+                                            GraphResponse response) {
+                                        // Application code
+                                        Log.i("fb", "fb user: "+ object);
+                                    }
+                                });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,link");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+
+                        //goMainMenu();
                     }
 
                     @Override
